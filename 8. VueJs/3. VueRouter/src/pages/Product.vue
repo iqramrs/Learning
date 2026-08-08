180187d22878c0b39ccfa055bae0ad683c8409a4
iqram <skyneca11@gmail.com>
2026-08-08 19:49:25 +0700

    feat: add product details display in Product page

--- 8. VueJs/3. VueRouter/src/pages/Product.vue
+++ 8. VueJs/3. VueRouter/src/pages/Product.vue
@@ -1,5 +1,20 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import products from '../data/products.json';
+
+const route = useRoute();
+// Find the product based on the route parameter
+// The route parameter is accessed using route.params.id, which is then converted to an integer using parseInt() to match the product ID in the products array. The find() method is used to search for the product with the matching ID.
+const product = products.find((product)=> product.id === parseInt(route.params.id));
+
+</script>
+
 <template>
     <div>
         <h1>Product Details</h1>
+        <p>Name: {{ product.name }}</p>
+        <p>Description: {{ product.description }}</p>
+        <p>Year: {{ product.year }}</p>
+        <p>Price: ${{ product.price }}</p>
     </div>
 </template>
